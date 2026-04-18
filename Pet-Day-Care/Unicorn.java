a9c0fa2b9d8157872130b461758472b6a49bcc81
Sahil <sahilakhtar995@icloud.com>
2026-04-18 10:24:22 +0100

    Update Unicorn.java

--- Pet-Day-Care/Unicorn.java
+++ Pet-Day-Care/Unicorn.java
@@ -1,1 +1,32 @@
+package petdaycare;
+
+//Modify the code below
+public class Unicorn extends Pet{
+    
+    private int glitterLevel;
+    public Unicorn(String name, int age, double baseFee, int glitterLevel){
+        super(name, age, baseFee);
+        
+        if (glitterLevel < 1 || glitterLevel > 5){
+            System.out.println("Error! Glitter Level must be between 1 - 5");
+        } else {
+            this.glitterLevel = glitterLevel;
+        }
+    }
+
+    public int getGlitterLevel(){
+        return glitterLevel;
+    }
+
+    @Override
+    public double calculateDailyCost(){
+        return baseFee + (glitterLevel * 30);
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("Neigh... ✨sparkle sparkle✨");
+    }
+
+}
 
