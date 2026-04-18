f10aa82e769ee9a0530637dcd404551101dd80dd
Sahil <sahilakhtar995@icloud.com>
2026-04-18 10:23:31 +0100

    Update NinjaCat.java

--- Pet-Day-Care/NinjaCat.java
+++ Pet-Day-Care/NinjaCat.java
@@ -1,1 +1,32 @@
+package petdaycare;
+
+//Modify the code below
+public class NinjaCat extends Pet{
+
+    private int stealthLevel;
+    public NinjaCat(String name, int age, double baseFee, int stealthLevel){
+        super(name, age, baseFee);
+
+        if (stealthLevel < 1 || stealthLevel > 10){
+            System.out.println("Error! Stealth Level must be between 1 - 10");
+        } else {
+            this.stealthLevel = stealthLevel;
+        }
+    }
+
+    public int GetStealthLevel(){
+        return stealthLevel;
+    }
+
+    @Override
+    public double calculateDailyCost(){
+        return baseFee - (stealthLevel * 10);
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("...");
+    }
+    
+}
 
