047b00245dfbb672970c52e828fff5a79a1805c0
Sahil <sahilakhtar995@icloud.com>
2026-04-18 10:23:56 +0100

    Update Pet.java

--- Pet-Day-Care/Pet.java
+++ Pet-Day-Care/Pet.java
@@ -1,1 +1,25 @@
+package petdaycare;
 
+//Modify the code below
+public abstract class Pet{
+
+    protected String name;
+    protected int age;
+    protected double baseFee;
+
+    public Pet(String name, int age, double baseFee){
+        this.name = name;
+        this.age = age;
+        this.baseFee = baseFee;
+    }
+
+    public abstract double calculateDailyCost();
+
+    public abstract void makeSound();
+
+    public void displayInfo(){
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+    
+}
