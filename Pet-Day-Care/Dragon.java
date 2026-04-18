d16f8866ac8c11028c1b29326d52bfe4972b0b1b
Sahil <sahilakhtar995@icloud.com>
2026-04-18 10:22:38 +0100

    Update Dragon.java

--- Pet-Day-Care/Dragon.java
+++ Pet-Day-Care/Dragon.java
@@ -1,1 +1,33 @@
+package petdaycare;
 
+//Modify the code below
+public class Dragon extends Pet{
+
+    private int fireBreathIntensity;
+    public Dragon(String name, int age, double baseFee, int fireBreathIntensity){
+        super(name, age, baseFee);
+
+        if (fireBreathIntensity < 1 || fireBreathIntensity > 10){
+            System.out.println("Error! Fire Breath Intensity must be a value between 0 - 10");
+        } else {
+            this.fireBreathIntensity = fireBreathIntensity;
+        }
+        
+    }
+
+    public int getFireBreathInensity(){
+        return fireBreathIntensity;
+    }
+
+    @Override
+    public double calculateDailyCost(){
+        return baseFee + (fireBreathIntensity * 50);
+    }
+
+    @Override
+    public void makeSound(){
+        System.out.println("ROAAAAR! 🔥");
+    }
+
+
+}
